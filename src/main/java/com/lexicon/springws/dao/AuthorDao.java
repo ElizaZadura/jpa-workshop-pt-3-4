a31374ed87b521df6220734dc4d6df1026126424
Eliza Zadura <eliza.zadura@gmail.com>
2024-04-16 10:30:00 +0200

    Add DAO classes for data access

---
+++ src/main/java/com/lexicon/springws/dao/AuthorDao.java
@@ -0,0 +1,16 @@
+package com.lexicon.springws.dao;
+
+import com.lexicon.springws.entity.Author;
+import java.util.List;
+
+public interface AuthorDao {
+    Author save(Author author);
+    Author findById(int id);
+    List<Author> findAll();
+    Author update(Author author);
+    void delete(Author author);
+    
+    // Additional query methods
+    Author findByEmail(String email);
+    List<Author> findByName(String name);
+} 
---
+++ src/main/java/com/lexicon/springws/dao/AuthorDaoImpl.java
@@ -0,0 +1,63 @@
+package com.lexicon.springws.dao;
+
+import com.lexicon.springws.entity.Author;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class AuthorDaoImpl implements AuthorDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    @Transactional
+    public Author save(Author author) {
+        entityManager.persist(author);
+        return author;
+    }
+
+    @Override
+    public Author findById(int id) {
+        return entityManager.find(Author.class, id);
+    }
+
+    @Override
+    public List<Author> findAll() {
+        TypedQuery<Author> query = entityManager.createQuery("SELECT a FROM Author a", Author.class);
+        return query.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public Author update(Author author) {
+        return entityManager.merge(author);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Author author) {
+        entityManager.remove(author);
+    }
+
+    @Override
+    public Author findByEmail(String email) {
+        TypedQuery<Author> query = entityManager.createQuery(
+            "SELECT a FROM Author a WHERE a.email = :email", Author.class);
+        query.setParameter("email", email);
+        return query.getResultList().stream().findFirst().orElse(null);
+    }
+
+    @Override
+    public List<Author> findByName(String name) {
+        TypedQuery<Author> query = entityManager.createQuery(
+            "SELECT a FROM Author a WHERE a.name LIKE :name", Author.class);
+        query.setParameter("name", "%" + name + "%");
+        return query.getResultList();
+    }
+} 
